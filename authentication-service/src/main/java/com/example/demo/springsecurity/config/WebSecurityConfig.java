345cab1f846b1ba2a798102a37692550b7acf1fb
smitchell1 <steve@platformconsulting.io>
2021-02-23 13:29:20 -0600

    Fixed Csrf token on custom login form

--- authentication-service/src/main/java/com/example/demo/springsecurity/config/WebSecurityConfig.java
+++ authentication-service/src/main/java/com/example/demo/springsecurity/config/WebSecurityConfig.java
@@ -5,6 +5,7 @@ import org.springframework.core.annotation.Order;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.builders.WebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.web.csrf.CookieCsrfTokenRepository;
 import org.springframework.security.web.util.matcher.AntPathRequestMatcher;
 
 @Order(-10)
@@ -42,8 +43,9 @@ public class WebSecurityConfig extends WebSecurityConfigurerAdapter {
                 .deleteCookies("JSESSIONID")
                 .permitAll()
                 .and()
-            .cors().disable()
-            .csrf().ignoringRequestMatchers((request) -> "/introspect".equals(request.getRequestURI()));
+            .csrf()
+                .csrfTokenRepository(CookieCsrfTokenRepository.withHttpOnlyFalse())
+                .ignoringRequestMatchers((request) -> "/introspect".equals(request.getRequestURI()));
     }
 
 }
