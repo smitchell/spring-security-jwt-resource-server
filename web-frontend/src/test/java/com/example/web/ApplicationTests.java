2329a72c442c18eabb3b180b2e4ee8665c287461
smitchell <stevecmitchell@gmail.com>
2021-06-07 09:00:09 -0500

    Added Spring Boot Wrapper

---
+++ web-frontend/src/test/java/com/example/web/ApplicationTests.java
@@ -0,0 +1,15 @@
+package com.example.web;
+
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+public class ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+
+}
